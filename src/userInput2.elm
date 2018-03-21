394c67e891353af83e5665164c949a48a553a75e
John Nicholas <themrtortoise@gmail.com>
2018-03-21 22:47:25 +0000

    keyboard input

---
+++ src/userInput2.elm
@@ -0,0 +1,61 @@
+import Html exposing (..)
+import Keyboard exposing (KeyCode)
+import Set exposing (..)
+
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+   }
+
+type alias Model =
+    { pressedKeys : Set KeyCode
+    }
+
+type Msg
+    = KeyChange Bool KeyCode
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        KeyChange pressed keyCode  ->
+            (handleKeyChange pressed keyCode model, Cmd.none)
+
+handleKeyChange : Bool -> KeyCode -> Model -> Model
+handleKeyChange pressed keyCode model =
+  let
+    fn = if pressed then Set.insert else Set.remove
+    pressedKeys = fn keyCode model.pressedKeys
+
+  in
+    { model | pressedKeys = pressedKeys }
+
+
+view : Model -> Html Msg
+view model =
+  let
+      keyString =
+        Set.map toString model.pressedKeys
+        |> Set.foldr String.append ""
+  in
+      div []
+          [
+            text keyString
+          ]
+
+init : (Model, Cmd Msg)
+init =
+    (Model Set.empty   , Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    let
+        keys = [ Keyboard.downs (KeyChange True)
+               , Keyboard.ups (KeyChange False)
+               ]
+    in
+    keys |> Sub.batch
---
+++ tests/Example.elm
@@ -0,0 +1,22 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+  describe "Sample Test Suite"
+        [ describe "Unit test examples"
+            [ test "Addition" <|
+                \() ->
+                    Expect.equal (3 + 7) 10
+            , test "String.left" <|
+                \() ->
+                    Expect.equal "a" (String.left 1 "abcdefg")
+            -- , test "This test should fail - you should remove it" <|
+            --     \() ->
+            --         Expect.fail "Failed as expected!"
+            ]
+        ]
